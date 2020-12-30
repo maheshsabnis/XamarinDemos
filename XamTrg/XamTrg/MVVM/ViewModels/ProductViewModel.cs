80ce04b9b95620818ea86383498b1204e6f06291
Mahesh Sabnis <sabnis_m@hotmail.com>
2020-12-30 12:19:39 +0530

    Add files via upload

--- XamTrg/XamTrg/MVVM/ViewModels/ProductViewModel.cs
+++ XamTrg/XamTrg/MVVM/ViewModels/ProductViewModel.cs
@@ -17,6 +17,7 @@ namespace XamTrg.MVVM.ViewModels
         private ProductInfo _Product;
         private ObservableCollection<ProductInfo> _Products;
         private ProductService productService;
+        private bool _IsAddEnabled;
 
 
         public ProductViewModel()
@@ -26,12 +27,15 @@ namespace XamTrg.MVVM.ViewModels
             Product = new ProductInfo();
             Products = new ObservableCollection<ProductInfo>();
             productService = new ProductService();
+            _IsAddEnabled = false;
             #endregion
 
 
             #region Configure Command Properties by passing private method references to it
             GetProductsCommand = new Command(GetData);
             AddProductCommand = new Command(AddProduct);
+            NavigateCommand = new Command(Navigate);
+
             #endregion
 
         }
@@ -39,6 +43,7 @@ namespace XamTrg.MVVM.ViewModels
         #region Command Properties
         public ICommand GetProductsCommand { get; private set; }
         public ICommand AddProductCommand { get; private set; }
+        public ICommand NavigateCommand { get; private set; }
         #endregion
 
 
@@ -69,24 +74,54 @@ namespace XamTrg.MVVM.ViewModels
             }
         }
 
-
+        public bool IsAddEnabled
+        {
+            get
+            {
+                return _IsAddEnabled;
+            }
+            set
+            {
+                _IsAddEnabled = value;
+                OnProprtyChanged("IsAddEnabled");
+            }
+        }
         #endregion
 
 
         #region Private one-way methods. These method will call to Services for Read/Write operations
         private async void GetData()
         {
+            // clear the Products collection
+            Products.Clear();
             var productsResponse = await productService.GetProductInfoDataAsync();
             foreach (ProductInfo prd in productsResponse)
             {
                 _Products.Add(prd); // call CollectionChanged and then PropertyChanged on ObservableCollection
+            }
+            if (_Products.Count >= 0)
+            {
+                IsAddEnabled = true;
             }
         }
 
         private async void AddProduct()
         {
             Product = await productService.PostProductInfoAsync(Product);
+            if (Product.ProductRowId > 0)
+            {
+                // navigate to the Product List View
+                await App.Current.MainPage.Navigation.PopModalAsync();
+            }
         }
+
+        private async void Navigate()
+        {
+            // naigate to Add Product Page
+            // App.Current.MainPage, the current page object
+            await App.Current.MainPage.Navigation.PushModalAsync(new Views.AddProduct());
+        }
+
         #endregion
 
     }
